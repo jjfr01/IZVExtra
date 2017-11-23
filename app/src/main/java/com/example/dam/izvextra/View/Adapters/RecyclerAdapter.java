f4889f7a18bb7a881083306ea32c123da5773620
Juanjo Fernández <fedezreilobajj@hotmail.com>
2017-11-23 10:45:23 +0100

    Punto de control

---
+++ app/src/main/java/com/example/dam/izvextra/View/Adapters/RecyclerAdapter.java
@@ -0,0 +1,82 @@
+package com.example.dam.izvextra.View.Adapters;
+
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.dam.izvextra.Model.Pojo.Excursion;
+import com.example.dam.izvextra.R;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ExcursionViewHolder> {
+
+    private ArrayList<Excursion> datos;
+    private Context context;
+
+    public RecyclerAdapter(ArrayList<Excursion> datos) {
+        this.datos = datos;
+    }
+
+    public RecyclerAdapter(ArrayList<Excursion> datos, Context context) {
+        this.datos = datos;
+        this.context = context;
+    }
+
+    @Override
+    public ExcursionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler, parent, false);
+
+        ExcursionViewHolder evh = new ExcursionViewHolder(itemView);
+
+        return evh;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerAdapter.ExcursionViewHolder holder, int position) {
+
+        Excursion exc = datos.get(position);
+
+        //holder.bindLugares(lgs);
+
+        holder.bindExcursion(exc);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+    public class ExcursionViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView tvPlace;
+        private CardView cv1;
+
+
+        public ExcursionViewHolder(View itemView) {
+            super(itemView);
+            tvPlace = (TextView) itemView.findViewById(R.id.tvPlace);
+            cv1 = (CardView) itemView.findViewById(R.id.cv1);
+        }
+
+        public void bindExcursion(final Excursion s) {
+            tvPlace.setText(s.getPlace());
+
+            cv1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, "Card: " + s.getPlace(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/com/example/dam/izvextra/View/MainActivity.java
+++ app/src/main/java/com/example/dam/izvextra/View/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.dam.izvextra.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.example.dam.izvextra.Model.Pojo.Excursion;
+import com.example.dam.izvextra.Model.Pojo.Group;
+import com.example.dam.izvextra.Model.Pojo.Teacher;
 import com.example.dam.izvextra.R;
+import com.example.dam.izvextra.View.Adapters.RecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +21,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerAdapter ra = new RecyclerAdapter(generarDatos());
+        rv.setAdapter(ra);
+
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+
+    }
+
+    private ArrayList<Excursion> generarDatos(){
+
+        ArrayList<Excursion> excs = new ArrayList<>();
+
+        for (int i = 0; i<2; i++){
+
+            Teacher tch = new Teacher("Juanjo", "Fernandez", 1);
+            ArrayList<Teacher> tchs = new ArrayList<>();
+            tchs.add(tch);
+            Group grp = new Group("A", 1);
+            ArrayList<Group> grps = new ArrayList<>();
+            grps.add(grp);
+            Calendar calendar = Calendar.getInstance();
+            Date date =  calendar.getTime();
+            Excursion exc = new Excursion(tchs, grps, "Prueba", "Casa", "11:15", date);
+
+            excs.add(exc);
+
+        }
+
+        return excs;
     }
 }
