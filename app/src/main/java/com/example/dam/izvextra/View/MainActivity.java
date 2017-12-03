2d71f48c117d2e0210e4d3025b4b513327026e18
Juanjo Fernández <fedezreilobajj@hotmail.com>
2017-12-03 12:02:05 +0100

    Revert "Punto de control"
    This reverts commit 52cb02da9587909f81b7dd0f721f67223773370c.

--- app/src/main/java/com/example/dam/izvextra/View/MainActivity.java
+++ app/src/main/java/com/example/dam/izvextra/View/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.dam.izvextra.View;
 
-
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -8,12 +8,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 
 import com.example.dam.izvextra.Model.Pojo.Excursion;
 import com.example.dam.izvextra.Model.Pojo.Group;
@@ -32,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private ActionBar actionbar;
     private DrawerLayout drawerlayout;
     private NavigationView navigationView;
+    private Excursion holderExcursion;
     private ArrayList<Excursion> excs = new ArrayList<>();
 
     private void init() {
